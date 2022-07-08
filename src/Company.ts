15b2eaba64f5b27e65c1bd95d9821a8b583cae53
Patryk Bogusz <b3.bogusz@gmail.com>
2022-07-08 19:00:47 +0200

    add User and Company class

--- src/Company.ts
+++ src/Company.ts
@@ -1,14 +1,16 @@
 import { faker } from '@faker-js/faker';
 
-export class User {
-    name: string;
+export class Company {
+    companyName: string;
+    catchPhrase: string;
     location: {
         lat: number;
         lng: number;
     }
 
     constructor() {
-        this.name = faker.name.firstName();
+        this.companyName = faker.company.companyName();
+        this.catchPhrase = faker.company.catchPhrase();
         this.location = {
             lat: +faker.address.latitude(),
             lng: +faker.address.longitude()
