fda7b123f78f7892825df940c2b7dd6d9444a1a5
Patryk Bogusz <b3.bogusz@gmail.com>
2022-07-09 08:45:41 +0200

    add CustomMap class and googleMap api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+/// <reference types="@types/google.maps" />
 import { User } from './User'
 import { Company } from './Company'
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
 const company = new Company();
+new CustomMap('map');
 
 console.log(user, company)
