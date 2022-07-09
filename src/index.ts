47ad610524a69396b9c9672917f3ae6b799af5f2
Patryk Bogusz <b3.bogusz@gmail.com>
2022-07-09 09:46:16 +0200

    add position marker from google api for user and company

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { CustomMap } from "./CustomMap";
 
 const user = new User();
 const company = new Company();
-new CustomMap('map');
+const customMap = new CustomMap('map');
 
-console.log(user, company)
+customMap.addMarker(user)
+customMap.addMarker(company)
